3e5124acd37c1eb0364e54830476acdf48d727f5
Guilherme <guilhermeferrazronha123@gmail.com>
2022-11-25 22:55:33 -0300

    Código que vai ser entregue (provavelmente)

--- main.c
+++ main.c
@@ -8,8 +8,7 @@ int main(){
     setlocale(LC_ALL, "Portuguese");
 
     int op;
-    double k, x, resultado;
-    double cosseno = 0;
+    double k, k2, x, resultado;
 
     while(1){
         system("cls");
@@ -22,7 +21,7 @@ int main(){
             case 1:
                 //Guilherme
                 system("cls");
-                printf("f(x) = x^k\n");
+                printf("f(x) = x^k\n\n");
 
                 printf("Digite um valor para k: ");
                 scanf("%lf", &k);
@@ -31,53 +30,61 @@ int main(){
 
                 resultado = pow(x, k-1) * k;
 
-                printf("Derivada = %.4lf\n", resultado);
+                printf("\nf'(x) = %.4lf\n", resultado);
                 system("pause");
                 break;
             case 2:
                 //Fábio e Larissa
-                system("cls");
-                printf("f'(x) = log_k(x)\n");
+                while(1) {
+                    system("cls");
+                    printf("f(x) = log_k(x)\n\n");
 
-
-
-                system("pause");
+                    printf("Digite o valor de k : ");
+                    scanf("%lf", &k);
+                    printf("Digite o valor de x : ");
+                    scanf("%lf", &x);
+                    if (x == 0 || k == 1) {
+                        printf("Por favor verifique o valor digitado e tente novamente \n");
+                        system("pause");
+                    } else {
+                        printf("\nf'(x): %.4lf\n", 1 / (x * log(k)));
+                        system("pause");
+                        break;
+                    }
+                }
                 break;
             case 3:
                 //Jonathan
                 system("cls");
-                printf("f'(x) = k^x\n");
+                printf("f(x) = k^x\n\n");
                 printf("Digite um valor para k: ");
                 scanf("%lf", &k);
                 printf("Digite um valor para x: ");
                 scanf("%lf", &x);
-                resultado = pow(k, x);
-                //falta arrumar a formula multiplicar por ln(k) - Jonathan
-                if(k<0){
-                    printf("Por favor, insira um valor positivo");
-                }else {
-                    printf("Derivada = %.4lf\n", resultado);
+                if(k < 0){
+                    k2 = k * -1;
+                }else{
+                    k2 = k;
                 }
-
+                resultado = pow(k, x) * log(k2);
+                printf("\nf'(x) = %.4lf\n", resultado);
                 system("pause");
                 break;
             case 4:
                 //Esther
                 system("cls");
-                printf("f'(x) = tg(x)\n");
-                printf("Digite um valor para x: \n");
+                printf("f(x) = tg(x)\n\n");
+                printf("Digite um valor para x: ");
                 scanf("%lf",&x);
-                cosseno = cos(x);
                 resultado = 1/pow(cos(x),2);
-                printf("f'(x) = %.4lf\n", resultado);
-
+                printf("\nf'(x) = %.4lf\n", resultado);
                 system("pause");
                 break;
             case 5:
                 exit(0);
             default:
                 system("cls");
-                printf("Digite uma opcao valida\n");
+                printf("Digite uma opção válida\n");
                 system("pause");
                 break;
         }
